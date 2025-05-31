dffd1c4328fef7f0880791270ef849642e3b9569
Vitor Gabriel Duarte de Oliveira <Oliveiravitorgabriel75@gmail.com>
2025-05-31 16:44:30 -0300

    implementada injeção de dependências

--- ApiPrimeiroSimulado/Program.cs
+++ ApiPrimeiroSimulado/Program.cs
@@ -1,6 +1,8 @@
 using ApiPrimeiroSimulado.Data;
 using Microsoft.EntityFrameworkCore;
 using ApiPrimeiroSimulado.Services.Produto;
+using ApiPrimeiroSimulado.Services.Transacoes;
+using ApiPrimeiroSimulado.Services.Usuario;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Injeção de dependência do service
-builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder.Services.AddScoped<IProdutoInterface, ProdutoService>();
+builder.Services.AddScoped<ITransacaoInterface, TransacaoService>();
+builder.Services.AddScoped<IUsuarioInterface, UsuarioService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
